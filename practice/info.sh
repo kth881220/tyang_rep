9cc9ee38e7fb9a65f25d750253b78ed0dc3c67da
root <root@DESKTOP-2V76E36.localdomain>
2026-06-04 00:44:26 +0900

    쉘 스크립트 연습 파일 추가

---
+++ practice/info.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "=== 시스템 정보 ==="
+echo "현재 위치:"
+pwd
+echo ""
+echo "파일 목록:"
+ls
+echo ""
+echo "현재 시간:"
+date
---
+++ practice/myscript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "내 이름은 tyang"
+echo "오늘 배운 것: 리눅스 기초"
+echo "파일 개수:"
+find . -type f | wc -l
