c5889d5bfc1ed7ee363f98c39cbf9c61362e4b83
root <root@DESKTOP-2V76E36.localdomain>
2026-06-04 01:18:14 +0900

    파이썬 연습

---
+++ practice/hello.py
@@ -0,0 +1,3 @@
+print("안녕하세요!")
+print(1 + 1)
+print("Python" + " 시작!")
---
+++ practice/mycode.py
@@ -0,0 +1,7 @@
+name = "tyang"
+job = "AI 개발자"
+
+print("이름:", name)
+print("목표:", job)
+print("1 + 2 =", 1 + 2)
+print("이름 글자수:", len(name))
